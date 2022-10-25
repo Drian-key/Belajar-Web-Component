f5e3889b22dbba99b3d6bc354189b43199278c62
Drian-key <riskidrian1@gmail.com>
2022-10-25 18:33:41 +0700

    Custom element dan shadow dom

---
+++ components.js
@@ -0,0 +1,20 @@
+console.log("Hello, web components");
+
+class MyComponent extends HTMLElement {
+    connectedCallback(){
+        this.innerHTML = `<h1>Hello From My Component</h1>`
+    }
+}
+
+customElements.define('my-component', MyComponent);
+
+const shadowRoot = document.querySelector('#root').attachShadow({mode: 'open'});
+shadowRoot.innerHTML = `
+<style type="text/css" media="screen">
+h1{
+    color: #fff;
+    background-color: blue;
+}
+</style>
+<h1>hi</h1>
+`;
